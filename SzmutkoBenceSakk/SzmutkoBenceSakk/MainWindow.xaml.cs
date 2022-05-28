bc40ba3d1381514818a18cd3599c3f7628f7e431
Citromszorny <92793113+Citromszorny@users.noreply.github.com>
2022-05-28 12:46:02 +0200

    Bábuk

--- SzmutkoBenceSakk/SzmutkoBenceSakk/MainWindow.xaml.cs
+++ SzmutkoBenceSakk/SzmutkoBenceSakk/MainWindow.xaml.cs
@@ -20,9 +20,41 @@ namespace SzmutkoBenceSakk
     /// </summary>
     public partial class MainWindow : Window
     {
+        private Chessboard _chessboard;
         public MainWindow()
         {
             InitializeComponent();
+            // 310 514
+            _chessboard = new Chessboard(this, 20, 500);
+        }
+
+        private void OnRadioClick(object sender, RoutedEventArgs e)
+        {
+            RadioButton button = (RadioButton)sender;
+            switch (button.Name)
+            {
+                case "lightPawnRB":
+                    _chessboard.CurrentPiece = Piece.LightPawn;
+                    break;
+                case "darkPawnRB":
+                    _chessboard.CurrentPiece = Piece.DarkPawn;
+                    break;
+                case "rookRB":
+                    _chessboard.CurrentPiece = Piece.Rook;
+                    break;
+                case "knightRB":
+                    _chessboard.CurrentPiece = Piece.Knight;
+                    break;
+                case "bishopRB":
+                    _chessboard.CurrentPiece = Piece.Bishop;
+                    break;
+                case "queenRB":
+                    _chessboard.CurrentPiece = Piece.Queen;
+                    break;
+                case "kingRB":
+                    _chessboard.CurrentPiece = Piece.King;
+                    break;
+            }
         }
     }
 }
