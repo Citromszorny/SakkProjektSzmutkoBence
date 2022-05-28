720419dce24be1db077303ab37843b23502c47e3
Citromszorny <92793113+Citromszorny@users.noreply.github.com>
2022-05-28 13:17:43 +0200

    Finomítgatások

--- SzmutkoBenceSakk/SzmutkoBenceSakk/Mozgas.cs
+++ SzmutkoBenceSakk/SzmutkoBenceSakk/Mozgas.cs
@@ -3,79 +3,80 @@ using System.Collections.Generic;
 using System.IO;
 using System.Net.Sockets;
 
-namespace SzmutkoBenceSakk;
-
-public class MoveCalculator
+namespace SzmutkoBenceSakk
 {
-    public static List<Position> GetValidMoves(Position from, Piece piece)
+    public class MoveCalculator
     {
-        List<Position> validMoves = new List<Position>();
-        piece.Moves.ForEach(m =>
+        public static List<Position> GetValidMoves(Position from, Piece piece)
         {
-            var temp = from.Clone();
-            for (var i = 0; i < m.Steps.Count; i++)
+            List<Position> validMoves = new List<Position>();
+            piece.Moves.ForEach(m =>
             {
-                var step = m.Steps[i];
-                if (step.Type == Type.SINGLE)
+                var temp = from.Clone();
+                for (var i = 0; i < m.Steps.Count; i++)
                 {
-                    if (i == m.Steps.Count - 1)
-                        validMoves.Add(add(temp, step.Direction, step.Amount));
-                    else
-                        add(temp, step.Direction, step.Amount);
-                }
-                else
-                {
-                    bool once = true;
-                    for (Position p = temp.Clone(); p.X is <= 8 and >= 1 && p.Y is <= 8 and >= 1; add(p, step.Direction, 1))
+                    var step = m.Steps[i];
+                    if (step.Type == Type.SINGLE)
                     {
-                        validMoves.Add(p.Clone());
+                        if (i == m.Steps.Count - 1)
+                            validMoves.Add(add(temp, step.Direction, step.Amount));
+                        else
+                            add(temp, step.Direction, step.Amount);
+                    }
+                    else
+                    {
+                        bool once = true;
+                        for (Position p = temp.Clone(); p.X is <= 8 and >= 1 && p.Y is <= 8 and >= 1; add(p, step.Direction, 1))
+                        {
+                            validMoves.Add(p.Clone());
+                        }
                     }
                 }
-            }
-        });
-        // gyalog nyitás kezelése
-        if (piece.Name == "sötét gyalog" && from.Row == 7)
-            validMoves.Add(new Position(from.X, 5));
-        if (piece.Name == "világos gyalog" && from.Row == 2)
-            validMoves.Add(new Position(from.X, 4));
-        // túlcsorduló pozíciók kezelése
-        return validMoves.FindAll(p => p.X > 0 && p.X < 9 && p.Y > 0 && p.Y < 9);
-    }
-
-    private static Position add(Position p, Direction d, int i)
-    {
-        switch (d)
-        {
-            case Direction.NORTH:
-                p.Y += i;
-                break;
-            case Direction.EAST:
-                p.X += i;
-                break;
-            case Direction.SOUTH:
-                p.Y -= i;
-                break;
-            case Direction.WEST:
-                p.X -= i;
-                break;
-            case Direction.NORTHEAST:
-                p.X += i;
-                p.Y += i;
-                break;
-            case Direction.SOUTHEAST:
-                p.X += i;
-                p.Y -= i;
-                break;
-            case Direction.SOUTHWEST:
-                p.X -= i;
-                p.Y -= i;
-                break;
-            case Direction.NORTHWEST:
-                p.X -= i;
-                p.Y += i;
-                break;
+            });
+            // gyalog nyitás kezelése
+            if (piece.Name == "sötét gyalog" && from.Row == 7)
+                validMoves.Add(new Position(from.X, 5));
+            if (piece.Name == "világos gyalog" && from.Row == 2)
+                validMoves.Add(new Position(from.X, 4));
+            // túlcsorduló pozíciók kezelése
+            return validMoves.FindAll(p => p.X > 0 && p.X < 9 && p.Y > 0 && p.Y < 9);
         }
 
-        return p;
+        private static Position add(Position p, Direction d, int i)
+        {
+            switch (d)
+            {
+                case Direction.NORTH:
+                    p.Y += i;
+                    break;
+                case Direction.EAST:
+                    p.X += i;
+                    break;
+                case Direction.SOUTH:
+                    p.Y -= i;
+                    break;
+                case Direction.WEST:
+                    p.X -= i;
+                    break;
+                case Direction.NORTHEAST:
+                    p.X += i;
+                    p.Y += i;
+                    break;
+                case Direction.SOUTHEAST:
+                    p.X += i;
+                    p.Y -= i;
+                    break;
+                case Direction.SOUTHWEST:
+                    p.X -= i;
+                    p.Y -= i;
+                    break;
+                case Direction.NORTHWEST:
+                    p.X -= i;
+                    p.Y += i;
+                    break;
+            }
+
+            return p;
+        }
     }
 }
--- SzmutkoBenceSakk/SzmutkoBenceSakk/Pozício.cs
+++ SzmutkoBenceSakk/SzmutkoBenceSakk/Pozício.cs
@@ -1,12 +1,37 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace SzmutkoBenceSakk
+﻿namespace SzmutkoBenceSakk
 {
-    internal class Pozício
+    public class Position
     {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public int Row => Y;
+        public int Column => X;
+
+        public Position(int x, int y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+
+        public int ToPixelX(int baseX)
+        {
+            return baseX + (X - 1) * 60;
+        }
+
+        public int toPixelY(int baseY)
+        {
+            return baseY - (Y - 1) * 60;
+        }
+
+        public Position Clone()
+        {
+            return new Position(X, Y);
+        }
+
+        public override string ToString()
+        {
+            return "" + (char)('A' + (Column - 1)) + Row;
+        }
     }
 }
